58be8d2974ef8fe4236c9bf0fa78fd7d2a294f3a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-12-05 11:34:20 +1100

    added server functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { text: "Hi there!", user: "Amanda", added: new Date() },
+  { text: "Hello World!", user: "Charles", added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log(messages);
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+/* GET NEW PAGE */
+router.get("/new", (req, res, next) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res, next) => {
+  const author = req.body.author;
+  const message = req.body.message;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
